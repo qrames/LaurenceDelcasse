c537bce1170954552c2a88d9ec9570fd73253b97
quentin <quentin@quentin.home>
2020-10-25 12:35:09 +0100

    email first part / no config smpt servise

--- accueil/urls.py
+++ accueil/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('thanks/', views.thanks, name='thanks'),
     path('', views.index, name='index'),
 ]
--- accueil/views.py
+++ accueil/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+from django.core.mail import send_mail
+
+from .forms import ContactForm
 
 # Create your views here.
+import logging
+logger = logging.getLogger(__name__)
 
 def index(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
+
+            recipients = ['quentinrames@gmail.com']
+            if cc_myself:
+                recipients.append(sender)
+
+            send_mail(subject, message, sender, recipients)
+            logger.error((subject, message, sender, recipients))
+            return HttpResponseRedirect('/thanks/')
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm()
+    return render(request, 'accueil/index.html', {'form': form})
+
+
+def thanks(request):
     context = {}
-    return render(request, 'accueil/index.html', context)
+    return render(request, 'accueil/thanks.html', context)
