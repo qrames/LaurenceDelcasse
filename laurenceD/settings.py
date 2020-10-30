0c61ed0c45ad605a5ee0bd660fb534000305ee05
quentin <quentin@quentin.home>
2020-10-30 17:16:35 +0100

    setting email smtp dont work

--- laurenceD/settings.py
+++ laurenceD/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .email_settings import EMAIL_BACKEND, EMAIL_HOST, EMAIL_USE_TLS
+from .email_settings import EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
