f9867f650c25c7fcc328f904589ec597bdc6e3a9
qrames <ramque@orange.fr>
2020-11-05 12:32:15 +0100

    no scroll effect on smartphone

--- accueil/static/accueil/js/index.js
+++ accueil/static/accueil/js/index.js
@@ -56,54 +56,53 @@ $( document ).ready(function() {
 				return [0,0,0];
 		}
 	}
+	if ($(window).width() >= 960) {
+		$document.scroll(function() {
 
-	$document.scroll(function() {
+			var $scrollBottom = $window.scrollTop() + $window.height();
 
-		var $scrollBottom = $window.scrollTop() + $window.height();
+			var heightEffect = 400;
+			$('.w').each(function(index){
+				//scroll image effect
+				var $this_images_top = $(this).offset().top;
 
-		var heightEffect = 400;
-		$('.w').each(function(index){
-			//scroll image effect
-			var $this_images_top = $(this).offset().top;
+				if ( $scrollBottom - $this_images_top + 100 > 0){
 
-			if ( $scrollBottom - $this_images_top + 100 > 0){
+					var effect = ($scrollBottom - $this_images_top)/window.innerHeight;
+					if (effect >= 0) {
+						$(this).children(".cadre-1").css("transform", tranform3d(xyzScroll(1,effect)))
+						$(this).children(".cadre-2").css("transform", tranform3d(xyzScroll(2,effect)))
+						$(this).children(".cadre-3").css("transform", tranform3d(xyzScroll(3,effect)))
+						$(this).children(".cadre-4").css("transform", tranform3d(xyzScroll(4,effect)))
+						$(this).children(".cadre-5").css("transform", tranform3d(xyzScroll(5,effect)))
+					}
 
-				var effect = ($scrollBottom - $this_images_top)/window.innerHeight;
-				if (effect >= 0) {
-					// $(this).css("margin-top", effect)
-					$(this).children(".cadre-1").css("transform", tranform3d(xyzScroll(1,effect)))
-					$(this).children(".cadre-2").css("transform", tranform3d(xyzScroll(2,effect)))
-					$(this).children(".cadre-3").css("transform", tranform3d(xyzScroll(3,effect)))
-					$(this).children(".cadre-4").css("transform", tranform3d(xyzScroll(4,effect)))
-					$(this).children(".cadre-5").css("transform", tranform3d(xyzScroll(5,effect)))
+					var $width = (100 - 20*($scrollBottom - $this_images_top)/heightEffect)
+					if ($width > 75){
+						// $(this).css("width", $width + "vw")
+					}
+
+					var opacity = ($scrollBottom - $this_images_top)/heightEffect
+					if (0 < opacity <= 1){
+						$(this).css("opacity", opacity )
+					}
 				}
 
-				var $width = (100 - 20*($scrollBottom - $this_images_top)/heightEffect)
-				if ($width > 75){
-					// $(this).css("width", $width + "vw")
-				}
+			});
 
-				var opacity = ($scrollBottom - $this_images_top)/heightEffect
-				if (0 < opacity <= 1){
-					$(this).css("opacity", opacity )
+
+			// h2 and p opacity
+			$('.opacity-scroll-magic').each(function(index){
+				if ($opacityScrollStartValues[index] < $scrollBottom < $opacityScrollStopValues[index]){
+					var opa = ($scrollBottom - $opacityScrollStartValues[index])/$heightEffectOpacity;
+					// opa = Math.pow(opa, 2);
+					opa = ((opa < 0) ? 0 : opa);
+					opa = ((opa > 1) ? 1 : opa);
+					// console.log(opa);
+					$(this).css("opacity", opa);
 				}
-			}
+			});
 
 		});
-
-
-		// h2 and p opacity
-		$('.opacity-scroll-magic').each(function(index){
-			if ($opacityScrollStartValues[index] < $scrollBottom < $opacityScrollStopValues[index]){
-				var opa = ($scrollBottom - $opacityScrollStartValues[index])/$heightEffectOpacity;
-				// opa = Math.pow(opa, 2);
-				opa = ((opa < 0) ? 0 : opa);
-				opa = ((opa > 1) ? 1 : opa);
-				// console.log(opa);
-				$(this).css("opacity", opa);
-			}
-		});
-
-	});
-
+	}
 });
